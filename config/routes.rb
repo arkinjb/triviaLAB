44461ff163690bc506106b200d62f15f48cd93c4
Preston Davies <pdavies88@gmail.com>
2015-09-04 11:59:37 -0400

    added in models and controllers in rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :questions, except: [:new, :edit, :show] do
+  resources :answers, except: [:new, :edit, :show]
+end
+root to: redirect('/questions')
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
